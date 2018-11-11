1d3c1818ed43d36ae86b001ffcc1a96a5bcd2b83
Stevenson Jean-Pierre <stevenson.jean-pierre@sage.com>
2018-11-11 18:05:16 -0500

    Removed Argo tunnel

--- build/startup.sh
+++ build/startup.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-
-export TUNNEL_ORIGIN_CERT="$(pwd)/argo_tunnel.pem"
-openssl enc -d -aes-256-cbc -in argo_tunnel.pem.enc -out argo_tunnel.pem -k $CERT_DECRYPT_KEY
-chmod +x ./cloudflared
-echo $ARGO_DOMAIN
-touch app.log tunnel.log
-nohup ./cloudflared --hostname $ARGO_DOMAIN http://localhost:9080 >> tunnel.log 2>&1 & echo $!
+touch app.log
 nohup ./input_tracker_app >> app.log 2>&1 & echo $!
-tail -qF *.log
+tail -qF app.log
